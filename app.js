2408ddd1435d478f6f6df755a205464d1d4ee4f0
singh-kanwarpreet <kanwarpreet388@gmail.com>
2026-01-26 16:36:38 +0530

    Added cors

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const authRouter = require("./routes/auth.routes");
 const eventRouter = require("./routes/event.routes");
+const cors = require('cors');
+
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
