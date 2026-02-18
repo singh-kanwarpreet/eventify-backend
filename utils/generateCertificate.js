9d142810c18f5fa16489bc79a67b00b83b82a671
singh-kanwarpreet <kanwarpreet388@gmail.com>
2026-02-18 23:14:11 +0530

    Refactored code for deployment

--- utils/generateCertificate.js
+++ utils/generateCertificate.js
@@ -2,15 +2,22 @@ const puppeteer = require("puppeteer");
 const certificateHTML = require("./certificateTemplate.js");
 
 module.exports = async (name, eventName) => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage", "--disable-gpu"],
+  });
 
   const page = await browser.newPage();
 
-  const html = certificateHTML(name, eventName, new Date().toDateString(), "Event Management Team");
+  const html = certificateHTML(
+    name,
+    eventName,
+    new Date().toDateString(),
+    "Event Management Team"
+  );
 
-  await page.setContent(html);
+  await page.setContent(html, { waitUntil: "networkidle0" }); 
 
-  // pdf saved in ram
   const pdfBuffer = await page.pdf({
     format: "A4",
     landscape: true,
