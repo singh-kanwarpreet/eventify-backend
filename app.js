0d450aa49bc93fc6a7500b2f9fffb13261673cfb
singh-kanwarpreet <kanwarpreet388@gmail.com>
2026-02-19 00:48:36 +0530

    Changed url

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://even-management-frontend.vercel.app/",
     credentials: true,
   }),
 );
